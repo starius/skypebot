5e370214d521887b2c2cca247a1fbcc6771a45bc
Boris Nagaev <bnagaev@gmail.com>
2013-03-16 18:35:36 +0400

    no uc changes in skype

--- sb.py
+++ sb.py
@@ -689,7 +689,6 @@ skype = Skype4Py.Skype(Events=MySkypeEvents())
 skype.Attach()
 for Chat in skype.RecentChats:
     if len(Chat.Members) > 2:
-        url2announces[UC_CHANGES].add(send_function(Chat))
         habr_announces.add(send_function(Chat))
 
 if IRC_ENABLED:
